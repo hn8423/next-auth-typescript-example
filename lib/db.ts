377f405a943e8ef15f07dc84aafed76c90eeb78c
hn8423 <rolenche@gmail.com>
2022-10-19 11:31:48 +0900

    mysql2 연결로직 풀로 변경하여 병렬형 접근으로 변경

--- lib/db.ts
+++ lib/db.ts
@@ -4,7 +4,7 @@ const mysql = require("mysql2")
 let db
 
 try {
-  db = mysql.createConnection({
+  db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
